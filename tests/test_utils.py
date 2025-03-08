6f748c4b7254c3227427c7bcf18fd4ef2c3fe0e8
himetik <himetik@tutamail.com>
2025-03-09 01:22:42 +0300

    .

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -13,6 +13,22 @@ def test_echo_with_float():
     assert echo(0.1) == 0.1
 
 
+def test_echo_int_addition():
+    assert echo(1 + 2) == 3
+
+
+def test_echo_int_subtraction():
+    assert echo(1 - 2) == -1
+
+
+def test_echo_int_devision():
+    assert echo(21 / 7) == 3
+
+
+def test_echo_int_multiplication():
+    assert echo(3 * 7) == 21
+
+
 def test_echo_with_string():
     assert echo("str") == "str"
 
