2b2c8bd273ae993ce435a277179ce256ee27da20
himetik <himetik@tutamail.com>
2025-03-09 03:22:42 +0300

    .

--- src/api.py
+++ src/api.py
@@ -1,7 +1,11 @@
+from fastapi import HTTPException
 from src.app import app
 from src.utils import echo
+from src.config import MAX_LENGTH
 
 
 @app.get("/echo/{x}")
 def get_echo(x: str):
+    if len(x) > MAX_LENGTH:
+        raise HTTPException(status_code=400, detail=f"Input too long. Max length is {MAX_LENGTH} characters.")
     return {"echo": echo(x)}
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,7 +1,19 @@
 import requests
+from src.config import MAX_LENGTH
 
 
 def test_get_echo():
-    response = requests.get("http://localhost:5000/echo/test")
+    url = "http://localhost:5000/echo/test"
+    response = requests.get(url)
     assert response.status_code == 200
-    assert response.json() == {"echo": "test"}
+    assert response.json() == {
+        "echo": "test"
+    }
+
+
+def test_get_echo_exceeded_length():
+    url = f'http://localhost:5000/echo/{"a" * (MAX_LENGTH + 1)}'
+    response = requests.get(url)
+    assert response.json() == {
+        "detail": "Input too long. Max length is 80 characters."
+    }
